3582fc4d0826905cf63fca3ec27e21d5097b0216
bad coder <2061469002@qq.com>
2018-02-10 15:44:54 +0800

    basically completed，can't handle the wrong input.

--- calculate/calculate.js
+++ calculate/calculate.js
@@ -1,12 +1,13 @@
 window.onload = function () {
   var wrap = document.getElementById("wrap");
-  var display = document.querySelector("#display");
   var content = document.querySelector("#content");
   var first = document.querySelector("#first-line");
   var keys = wrap.getElementsByTagName("div");
   var equal = document.getElementById("equal");
   var del = document.querySelector("#del");
   var percent = document.querySelector("#percent");
+  var old_fSize = parseInt(window.getComputedStyle(first).fontSize);
+  var new_fSize = parseInt(window.getComputedStyle(first).fontSize);
 
   percent.onclick = keyClick;
   del.onclick = function () {
@@ -14,22 +15,26 @@ window.onload = function () {
   };
 
   equal.onclick = function () {
+    new_fSize = old_fSize;
+    first.style.fontSize = old_fSize + "px";
     var text = first.innerHTML.replace(/[<br>]/g, "");
+    text = convert_per(text);
+    text = result(text);
     var new_text = "";
     var flag = true;
-    if(text.length <= 24) {
-      new_text = text ;
+    if (text.length <= 24) {
+      new_text = text;
     } else {
-      for(var i = 0, j = 24; j <= text.length ;) {
-      new_text += text.substring(i, j) + "<br>";
-      i = j + 1;
-      if(j * 2 >= text.length && flag) {
-        j = text.length;
-        flag = false;
-      } else {
-        j *= 2;
+      for (var i = 0, j = 24; j <= text.length;) {
+        new_text += text.substring(i, j) + "<br>";
+        i = j + 1;
+        if (j * 2 >= text.length && flag) {
+          j = text.length;
+          flag = false;
+        } else {
+          j *= 2;
+        }
       }
-    }
     }
     content.innerHTML += new_text + "<br><br>";
     first.style.fontSize = parseInt(window.getComputedStyle(first).fontSize) - 26 + "px";
@@ -54,6 +59,10 @@ window.onload = function () {
       first.innerText = first.innerText + value;
     } else {
       first.innerHTML += "<br>" + value;
+      if (new_fSize > 20) {
+        new_fSize -= 8;
+        first.style.fontSize = new_fSize + "px";
+      }
     }
     // set the bottom size of the content area
     content.style.bottom = first.offsetHeight + "px";
@@ -61,6 +70,68 @@ window.onload = function () {
 
   //evaluate the result according to the expression
   function result(expression) {
-    
+    //divide expression into numbers and operations
+    var num = expression.split(/[-+÷×]/g);
+    var operations = expression.split(/\d+\.?\d*/g);
+    var pre_num, next_num;
+    var minus_sign = false;
+    if (operations[0] === "-") {
+      minus_sign = true;
+      operations.splice(0, 1);
+      operations.replace(/[-+]/g, function (match, pos, originalText) {
+        switch (match) {
+          case "-": {
+            return "+";
+          }
+          case "+": {
+            return "-";
+          }
+        }
+      });
+    }
+    num = del_empty(num);
+    operations = del_empty(operations);
+    for (var i = 0; i < operations.length; i++) {
+      pre_num = parseFloat(num[i]);
+      next_num = parseFloat(num[i + 1]);
+      if (operations[i] === "×" || operations[i] === "÷") {
+        if (operations[i] === "÷") {
+          num[i] = pre_num / next_num + "";
+        } else {
+          num[i] = pre_num * next_num + "";
+        }
+        num.splice(i + 1, 1);
+        operations.splice(i, 1);
+        i--;
+        console.log(num, operations);
+      }
+    }
+    while (num.length !== 1) {
+      if (operations[0] === "+") {
+        num[0] = parseFloat(num[0]) + parseFloat(num[1]) + "";
+      } else {
+        num[0] = parseFloat(num[0]) - parseFloat(num[1]) + "";
+      }
+      num.splice(1, 1);
+      operations.splice(0, 1);
+    }
+    if (minus_sign) {
+      return "-" + num.toString();
+    }
+    return num.toString();
+  }
+
+  // deletes an empty character in an Array
+  function del_empty(arr) {
+    return arr.filter(function (item, index, array) {
+      return (item !== "" && item !== undefined);
+    })
+  }
+
+  // convert a percent sign to a decimal
+  function convert_per(expression) {
+    return expression.replace(/\d+\.?\d*%/g, function (match, pos, originalText) {
+      return parseInt(match) / 100;
+    })
   }
 };
