4e0e8389a66f866e76df66733087a3b9e14d0b0b
bad coder <2061469002@qq.com>
2018-03-03 18:17:10 +0800

    add button demo2

---
+++ button/js/gradient.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+  let button = document.querySelector('.gradient');
+  button.onmousemove = (e) => {
+    const x = e.pageX - e.target.offsetLeft;
+    const y = e.pageY - e.target.offsetTop;
+    e.target.style.setProperty('--x', `${ x }px`);
+    e.target.style.setProperty('--y', `${ y }px`);
+  }
+};
